1b5c6a97bad90feaef14e3461d2dae345bd54f14
Anwesh43 <anweshthecool0@gmail.com>
2021-05-21 01:00:40 +0530

    adding Canvas extension methods

--- xarcsweepview/src/main/java/com/example/xarcsweepview/XArcSweepView.kt
+++ xarcsweepview/src/main/java/com/example/xarcsweepview/XArcSweepView.kt
@@ -30,3 +30,37 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawXArcSweep(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val sf4 : Float = sf.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f, 1f - 2 * j)
+        save()
+        rotate(rot * sf2)
+        drawLine(0f, -size * sf1, 0f, size * sf1, paint)
+        restore()
+        paint.style = Paint.Style.STROKE
+        drawArc(RectF(-size, -size, size, size), rot / 2, rot * sf3, false, paint)
+        paint.style = Paint.Style.FILL
+        drawArc(RectF(-size, -size, size, size), rot / 2, rot * sf4, true, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawXASNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawXArcSweep(scale, w, h, paint)
+}
