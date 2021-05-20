ba428f932a21cbb9b8aeff0488be3539ad05ec3a
Anwesh43 <anweshthecool0@gmail.com>
2021-05-21 00:44:59 +0530

    adding constants and libraries

--- xarcsweepview/src/main/java/com/example/xarcsweepview/XArcSweepView.kt
+++ xarcsweepview/src/main/java/com/example/xarcsweepview/XArcSweepView.kt
@@ -1,2 +1,27 @@
 package com.example.xarcsweepview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#3F51B5",
+    "#BF360C",
+    "#311B92",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 3.2f
+val delay : Long = 20
+val rot : Float = 90f
+val backColor : Int = Color.parseColor("#BDBDBD")
